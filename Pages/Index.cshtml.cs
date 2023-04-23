3c1ac8895eb702ca6aac1114a1bba502c03c2f96
Tekcoder <abdurroheem20@gmail.com>
2023-04-23 16:56:15 +0100

    created a dotnet app

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Live_Sessions_With_Ibrahim_Olanigan.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
